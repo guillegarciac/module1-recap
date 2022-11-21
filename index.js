62397bcb5bc04b49cd73aa1d71ab1dfa20fce290
guille garcia <ggarciacardiel@gmail.com>
2022-11-21 18:23:43 +0100

    iteration 2, 3 and 6 (only mobile)

--- index.js
+++ index.js
@@ -1,78 +1,109 @@
+//landing popups
+function blueAlert() {
+  alert("Your choice: to remain in contented ignorance");
+}
+
 /// Iteration 2: getMaxNum
 
-/* Your code goes here */
+function getMaxNum (array) {
+  let maxNumber = array[0];
+  if (typeof maxNumber === 'string') {
+    maxNumber = maxNumber.length;
+  } else if (typeof maxNumber === 'object') {
+    maxNumber = maxNumber[0] * maxNumber[1] * maxNumber[2]
+  }
+  if (array.length === 0) return null;
+  if (array.length === 1) return maxNumber;
 
-// Iteration 2 tests:
-// console.log(getMaxNum([45, 67, 12, 34])) // Should return 67
-// console.log(getMaxNum(['hello', [0, 1, 2], 34, 45, 12, 0])) // Should return 45
+  for (let i = 0; i < array.length; i++) {
+    let element = array[i];
+    if (typeof element === 'string') {
+      element = element.length;
+    } else if (typeof element === 'object') {
+      element = element[0] * element[1] * element[2]
+    }
+    if (element > maxNumber) {
+      maxNumber = element;
+    }
+  }
+  return maxNumber;
+};
 
-
-
+console.log(getMaxNum([45, 67, 12, 34])) // Should return 67
+console.log(getMaxNum(['hello', [5, 400, 2], 34, 45, 12, 0])) // Should return 45
 
 /// Iteration 3: changeItem
 
-/* Your code goes here */
+function changeItem (array, num1, num2) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== num1) {
+      newArray.push(array[i])
+    }
+    else if (array[i] === num1) {
+      newArray.push(num2)
+    }
+  }
+  return newArray;
+}
 
-// Iteration 3 tests:
-// console.log(changeItem([3, 5, 7], 3, 4)) // Should return [4,5,7]
-// console.log(changeItem([17, 18, 6, 18, 3], 18, 2)) // Should return [17, 2, 6, 2, 3];
-// console.log(changeItem([1, 1, 3, 2, 4, 1, 1], 1, 8)) // Should return [8, 8, 3, 2, 4, 8, 8] 
-
-
-
+console.log(changeItem([3, 5, 7], 3, 4)) // Should return [4,5,7]
+console.log(changeItem([17, 18, 6, 18, 3], 18, 2)) // Should return [17, 2, 6, 2, 3];
+console.log(changeItem([1, 1, 3, 2, 4, 1, 1], 1, 8)) // Should return [8, 8, 3, 2, 4, 8, 8] 
 
 /// Iteration 4: orderByDate
 
-/* Your code goes here */
+function orderByDate (array) {
 
-// Iteration 4 tests:
-// const birthdays = [
-//   {
-//     name: 'Bob',
-//     year: 1989,
-//   },
-//   {
-//     name: 'Rosita',
-//     year: 1990,
-//   },
-//   {
-//     name: 'Stevie',
-//     year: 1988,
-//   },
-//   {
-//     name: 'Marlon',
-//     year: 1979,
-//   },
-// ]
-// const importantDates = [
-//   {
-//     title: 'Wedding aniversary',
-//     year: 2014,
-//   },
-//   {
-//     name: "Daddy's birthday",
-//     year: 2013,
-//   },
-//   {
-//     name: 'Graduation party',
-//     year: 2019,
-//   }
-// ]
-// console.log(orderByDate(birthdays)) // Should return them ordered 1979, 1988, 1989, 1990
-// console.log(orderByDate(importantDates)) // Should return them ordered 2013, 2014, 2019
+}
 
+const birthdays = [
+  {
+    name: 'Bob',
+    year: 1989,
+  },
+  {
+    name: 'Rosita',
+    year: 1990,
+  },
+  {
+    name: 'Stevie',
+    year: 1988,
+  },
+  {
+    name: 'Marlon',
+    year: 1979,
+  },
+]
 
+const importantDates = [
+  {
+    title: 'Wedding aniversary',
+    year: 2014,
+  },
+  {
+    name: "Daddy's birthday",
+    year: 2013,
+  },
+  {
+    name: 'Graduation party',
+    year: 2019,
+  }
+]
+
+console.log(orderByDate(birthdays)) // Should return them ordered 1979, 1988, 1989, 1990
+console.log(orderByDate(importantDates)) // Should return them ordered 2013, 2014, 2019
 
 
 /// Iteration 5: new machine gun
 
 /* Your code goes here */
 
-// Iteration 5 tests
-// const machineGun = new Weapon('Machine gun', 90, 50);
-// console.log('Begin:')
-// machineGun.shoot();
-// setTimeout(() => machineGun.stopShooting(), 1000);
-// setTimeout(() => machineGun.reload(35), 1500);
+/// Iteration 5 tests
+const machineGun = new Weapon('Machine gun', 90, 50);
+console.log('Begin:')
+machineGun.shoot();
+setTimeout(() => machineGun.stopShooting(), 1000);
+setTimeout(() => machineGun.reload(35), 1500);
 
 
